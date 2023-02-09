da9e97c19184d750e43fa6bba3b377bca7532a8d
K8bus <K8bus@proton.me>
2023-02-09 21:23:46 +0100

    Cos

--- src/main/java/com/ancientmodders/forfaitednature/mixin/ForfaitedNature.java
+++ src/main/java/com/ancientmodders/forfaitednature/mixin/ForfaitedNature.java
@@ -1,6 +1,5 @@
-package net.fabricmc.example.mixin;
+package com.ancientmodders.forfaitednature.mixin;
 
-import net.fabricmc.example.ExampleMod;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -8,9 +7,9 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(TitleScreen.class)
-public class ExampleMixin {
+public class ForfaitedNature {
 	@Inject(at = @At("HEAD"), method = "init()V")
 	private void init(CallbackInfo info) {
-		ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
+		com.ancientmodders.forfaitednature.ForfaitedNature.LOGGER.info("This line is printed by an forfaited nature mod mixin!");
 	}
 }
